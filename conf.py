f7ccd6a6730f1fe7a572afe7e1b6c0b51e98340d
Alfa4026 <alifalamsyah2004@gmail.com>
2025-09-27 01:17:58 +0700

    Initial docs

---
+++ conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Belajar'
+copyright = '2025, Alfa'
+author = 'Alfa'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'id'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ index.md
@@ -0,0 +1,11 @@
+# 📚 Dokumentasi Belajar Alfa
+
+Selamat datang di kumpulan dokumentasi belajar saya.  
+Pilih topik di bawah ini:
+
+```{toctree}
+:maxdepth: 2
+
+tmux
+python
+federated_learning
