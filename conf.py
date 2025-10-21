790ecabb357f4143574f8024d6a5dc43dd1604d6
Alif Alamsyah <148669647+Alfa4026@users.noreply.github.com>
2025-10-21 11:18:58 +0700

    Configure source_suffix for reStructuredText and Markdown
    Added source_suffix configuration for file types.

--- conf.py
+++ conf.py
@@ -16,6 +16,11 @@ release = '1.0'
 
 extensions = ["myst_parser"]
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
